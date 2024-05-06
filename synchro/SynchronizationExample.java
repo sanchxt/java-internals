938e63fa6b932539f8d6dcb43c4aeacc7374a47b
sanchxt <tpbeast007@gmail.com>
2024-05-07 02:33:33 +0530

    fix code

--- synchro/SynchronizationExample.java
+++ synchro/SynchronizationExample.java
@@ -1,9 +1,5 @@
 package synchro;
 
-import synchro.Counter;
-import synchro.IncrementThread;
-import synchro.DecrementThread;
-
 public class SynchronizationExample {
     public static void main(String[] args) throws InterruptedException {
         Counter counter = new Counter();
